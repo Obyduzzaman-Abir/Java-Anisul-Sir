fd031ff138a47ea418c7d0fbdbc239480e103cb0
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2024-11-29 20:55:04 +0600

    Added 61 num Program

---
+++ src/Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int sum=0,temp,num,rem;
+        num=input.nextInt();
+        temp=num;
+        for (int i=0;temp !=0;i++)
+        {
+            rem=temp%10;
+            sum=sum*10+rem;
+            temp=temp/10;
+        }
+
+        if (sum==num)
+        {
+            System.out.println("This is a Plindrome");
+        }
+        else
+        {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
